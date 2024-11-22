0a6d99d61816bf68c25d196b77f06ed423f06125
Juliia Prokopenko <atlasovaa13@gmail.com>
2024-11-22 16:47:56 +0100

    fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -14,15 +14,21 @@ type DataResponse = {
   total_pages: number;
 };
 
-const fetchImages = async (query: string, page: number) => {
-  const response = await axios.get(`https://api.unsplash.com/search/photos`, {
-    params: {
-      query,
-      page,
-      per_page: 9,
-      client_id: 'alWp6hurEvK-MK92m5HZv1-1euEpnmixSMj2njOszLo',
-    },
-  });
+const fetchImages = async (
+  query: string,
+  page: number
+): Promise<DataResponse> => {
+  const response = await axios.get<DataResponse>(
+    `https://api.unsplash.com/search/photos`,
+    {
+      params: {
+        query,
+        page,
+        per_page: 9,
+        client_id: 'alWp6hurEvK-MK92m5HZv1-1euEpnmixSMj2njOszLo',
+      },
+    }
+  );
   return response.data;
 };
 
